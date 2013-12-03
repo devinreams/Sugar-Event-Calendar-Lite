0c03c72fbc5d51773cd348e58b5ed25958bac92c
Devin Reams <devin@crowdfavorite.com>
2013-12-03 08:01:46 -0700

    add stub of sc_events_list shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -5,3 +5,8 @@ function sc_events_calendar_shortcode( $atts, $content = null ) {
 	return '<div id="sc_calendar_wrap">' . sc_get_events_calendar() . '</div>';
 }
 add_shortcode( 'sc_events_calendar', 'sc_events_calendar_shortcode' );
+
+function sc_events_list_shortcode( $atts, $content = null ) {
+	return '<div id="sc_events_list_wrap">' . sc_get_events_list() . '</div>';
+}
+add_shortcode( 'sc_events_list', 'sc_events_list_shortcode' );
